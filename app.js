e51d38dfef465cee1cf43f2ff981d7de4c3b1214
Koch Gustavo <a371634@europe.bmw.corp>
2022-08-01 21:50:52 +0200

    projections for avoiding _id in calenderItems2

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-app.use('/calender', calenderItemRouter);
+app.use('/calendar', calenderItemRouter);
 app.use('/contacts', contactsRouter);
 app.use('/tasks', tasksRouter);
 app.use('/activityList', activityListRouter);
