2e2ba1bd1b0b17cc469498a7657846717625e3cd
Koch Gustavo <a371634@europe.bmw.corp>
2022-08-11 14:42:38 +0200

    commit before for netlify

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 const corsOptions ={
+    origin:
+    
+        'https://projectberlincalender.netlify.app/'
+,
     methods :["GET", "PUT", "POST","DELETE" ],
     credentials:true,            //access-control-allow-credentials:true
     
